a7e613188641a4f68918f832d558f94736948550
Duvan manzano <jhorman980815@gmail.com>
2025-09-20 12:59:50 -0500

    feat: implement calculate Working Date

---
+++ src/internal/core/ports/services/iapp.service.ts
@@ -0,0 +1,20 @@
+export abstract class IAppService {
+  /**
+   * Returns a greeting message.
+   */
+  abstract getHello(): string;
+
+  /**
+   * Calculates a future or past date based on working days and hours, considering holidays.
+   * @param date The starting date.
+   * @param days The number of working days to add.
+   * @param hours The number of working hours to add.
+   * @returns A promise that resolves to the calculated date.
+   * @throws BadRequestException if the input date is invalid or if both days and hours are zero.
+   */
+  abstract calculateWorkingDate(params: {
+    days: number;
+    hours: number;
+    date?: string;
+  }): Promise<Date>;
+}
--- src/internal/core/services/app/app.service.ts
+++ src/internal/core/services/app/app.service.ts
@@ -1,8 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { IHolidayClient } from '../../ports/clients/IHoliday.client';
+import { IAppService } from '../../ports/services/iapp.service';
+import {
+  addWorkingDays,
+  addWorkingHours,
+  setToPreviousWorkingTime,
+} from '../../utils/working-date.utils';
 
 @Injectable()
-export class AppService {
+export class AppService implements IAppService {
+  constructor(private readonly holidayClient: IHolidayClient) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async calculateWorkingDate(params: {
+    days: number;
+    hours: number;
+    date?: string;
+  }): Promise<Date> {
+    const { days, hours, date } = params;
+    if (days === 0 && hours === 0) {
+      throw new BadRequestException('Days and hours cannot both be zero');
+    }
+
+    const initialDate = date ? new Date(date) : new Date();
+    if (isNaN(initialDate.getTime())) {
+      throw new BadRequestException('Invalid date format');
+    }
+
+    const holidays = await this.holidayClient.getHolidays(
+      initialDate.getFullYear(),
+    );
+
+    const startDate = setToPreviousWorkingTime(initialDate, holidays);
+    let responseDate = startDate;
+
+    if (days > 0) {
+      responseDate = addWorkingDays(startDate, days, holidays);
+    }
+
+    if (hours > 0) {
+      responseDate = addWorkingHours(responseDate, hours, holidays);
+    }
+
+    return responseDate;
   }
 }
